77071fd0aa9b40fe9b6098f1280dce5f8a06ba52
Tomivesco <103777623+Tomivesco@users.noreply.github.com>
2023-11-27 19:33:55 -0300

    todo ok

---
+++ Assets/Runner/Scripts/alertadesactivada2.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alertadesactivada2 : MonoBehaviour
+{
+    public bool alertaon2 = false;
+    // Start is called before the first frame update
+    public float tiempocorriendo = 0;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alertaon2 == false)
+        {
+            OcultarMiObjeto();
+        }
+
+        if (alertaon2 == true)
+        {
+            tiempocorriendo += Time.deltaTime;
+            MostrarMiObjeto();
+            if (tiempocorriendo >= 5)
+            {
+                alertaon2 = false;
+                tiempocorriendo = 0;
+
+            }
+        }
+    }
+
+    public void OcultarMiObjeto()
+    {
+        // Obtén el componente Renderer del objeto actual
+        Renderer miRenderer = GetComponent<Renderer>();
+
+        // Verifica si el componente Renderer existe y está habilitado
+        if (miRenderer != null && miRenderer.enabled)
+        {
+            // Desactiva el renderizado del objeto
+            miRenderer.enabled = false;
+        }
+    }
+    public void MostrarMiObjeto()
+    {
+        // Obtén el componente Renderer del objeto actual
+        Renderer miRenderer = GetComponent<Renderer>();
+
+        // Verifica si el componente Renderer existe y está deshabilitado
+        if (miRenderer != null && !miRenderer.enabled)
+        {
+            // Activa el renderizado del objeto
+            miRenderer.enabled = true;
+        }
+    }
+}
--- Assets/Runner/Scripts/generador.cs
+++ Assets/Runner/Scripts/generador.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class generador : MonoBehaviour
 {
+
     public GameObject enemigoPrefab;
     public Transform[] carriles;
     public float tiempoEntreGeneracion = 2.0f;
     public float rotacionXEnCarril0 = -30; // Ajusta el valor según tus necesidades
     public float rotacionZEnCarril2 = 25;
     private float tiempoUltimaGeneracion;
+    public float tiempocorriendo = 0;
+    public alertadesactivada a;
+    public alertadesactivada1 b;
+    public alertadesactivada2 c;
 
     void Start()
     {
         tiempoUltimaGeneracion = Time.time;
+        a = FindObjectOfType<alertadesactivada>();
+        b = FindObjectOfType<alertadesactivada1>();
+        c = FindObjectOfType<alertadesactivada2>();
     }
 
     void Update()
@@ -22,25 +30,34 @@ public class generador : MonoBehaviour
         {
             GenerarEnemigo();
             tiempoUltimaGeneracion = Time.time;
+
         }
     }
 
     void GenerarEnemigo()
     {
+
         int carrilAleatorio = Random.Range(0, carriles.Length);
         Vector3 posicionGeneracion = carriles[carrilAleatorio].position;
         Quaternion rotacion = Quaternion.identity;
 
         if (carrilAleatorio == 0)
         {
+
+            a.alertaon = true;
             rotacion = Quaternion.Euler(0, 0, rotacionXEnCarril0);
+
         }
         else if (carrilAleatorio == 2)
         {
+            c.alertaon2 = true;
             rotacion = Quaternion.Euler(0, 0, rotacionZEnCarril2);
+        }
+        if (carrilAleatorio == 1)
+        {
+            b.alertaon1 = true;
         }
 
         Instantiate(enemigoPrefab, posicionGeneracion, rotacion);
     }
 }
-
